53f6bd2cee201eecad158f8ad53445ab532b39a3
mrchvz <chavezmmarie@gmail.com>
2021-03-12 11:20:31 -0800

    react tutorial

---
+++ holis.js
@@ -0,0 +1,25 @@
+// JSX
+const holis = <h1> Holis </h1>
+
+// JS
+const holis = React.createElement(
+  'h1',
+  {},
+  'Holis'
+);
+
+// JSX
+const list = (
+  <ul>
+    <li>
+      <a href='gatopan.com/diewo'>
+        Diewo
+      </a>
+    </li>
+  </ul>
+)
+
+// JS
+const list = React.createElement(
+  'ul',
+)
